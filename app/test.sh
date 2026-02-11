f26b8653af1529f013c9ff4660e0b22423cff66b
nik-markovic <nikola.m@gmail.com>
2026-02-11 15:53:38 -0600

    kill stale processes in test.sh

--- app/test.sh
+++ app/test.sh
@@ -6,7 +6,7 @@
 # This software is licensed under terms that can be found in the LICENSE file
 # in the root directory of this software component.
 # If no LICENSE file comes with this software, it is provided AS-IS.
-
+killall python3
 weston_user=$(ps aux | grep '/usr/bin/weston '|grep -v 'grep'|awk '{print $1}')
 FRAMEWORK=$1
 echo "stai wrapper used : "$FRAMEWORK
@@ -15,7 +15,7 @@ source $CONFIG
 
 # Original ST application launch command, using the stai wrapper and the config_board_*.sh variables
 if [ "$RUN_ST" = "1" ]; then
-  cmd="python3 /usr/local/x-linux-ai/image-classification/stai_mpu_image_classification.py -m /usr/local/x-linux-ai/image-classification/models/$IMAGE_CLASSIFICATION_MODEL -l /usr/local/x-linux-ai/image-classification/models/$IMAGE_CLASSIFICATION_LABEL.txt --framerate $DFPS --frame_width $DWIDTH --frame_height $DHEIGHT $OPTIONS"
+    cmd="python3 /usr/local/x-linux-ai/image-classification/stai_mpu_image_classification.py -m /usr/local/x-linux-ai/image-classification/models/$IMAGE_CLASSIFICATION_MODEL -l /usr/local/x-linux-ai/image-classification/models/$IMAGE_CLASSIFICATION_LABEL.txt --framerate $DFPS --frame_width $DWIDTH --frame_height $DHEIGHT $OPTIONS"
 else
   cmd="python3 /home/root/app/test.py"
 fi
