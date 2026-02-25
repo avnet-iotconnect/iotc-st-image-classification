57d79c56b50f25b8b8a8e457239f679e5ebc2112
nik-markovic <nikola.m@gmail.com>
2026-02-25 10:49:35 -0600

    Fix issue where camera is not initialized

--- app/app.sh
+++ app/app.sh
@@ -6,6 +6,9 @@ if [ -f ~/.aws-env.sh ]; then
   source  ~/.aws-env.sh
 fi
 
+CONFIG_BOARD=$(find /usr/local/x-linux-ai -name "config_board_*.sh")
+source $CONFIG_BOARD
+
 pushd ~/app/ >/dev/null
 source ~/.venv-staiicdemo/bin/activate
 python3 app.py "$@"
